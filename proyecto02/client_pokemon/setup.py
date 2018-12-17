1534b4cb2646b53b32803fd37711e29792cdaab6
fernandagch <fernanda@gendra.mx>
2018-12-16 22:37:25 -0600

    [MODIFIED] Reestructure

---
+++ proyecto02/client_pokemon/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='proyecto02',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/Ferch2109/Redes/tree/master/proyecto02',
+    license='',
+    author='Gonzalez Chavez María Fernanda, Lezama Hernandez María Ximena',
+    author_email='fernandagch@ciencias.unam.mx, lezama@ciencias.unam.mx',
+    description='Simple app that simules the pokemon\'s game to catch pokemons.'
+)
